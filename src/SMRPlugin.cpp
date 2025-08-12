bf6e68cf1ac87600336680a144622bb9e3ac9da6
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-08-12 22:43:37 +0100

    Initial commit

---
+++ src/SMRPlugin.cpp
@@ -0,0 +1,11 @@
+#include "SMRPlugin.hpp"
+
+SMRPlugin::SMRPlugin(void) : CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE, vsmr::PLUGIN_NAME, vsmr::PLUGIN_VERSION, vsmr::PLUGIN_DEVELOPERS, vsmr::PLUGIN_COPYRIGHT)
+{
+    // yoyo I'm ALIVEEE
+}
+
+SMRPlugin::~SMRPlugin()
+{
+    // yoo I'm dead now
+}
