eb01d5d4154addb9677b69a510a986db7d25a9a9
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-08-12 20:54:54 +0100

    update bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-conan install . -s arch=x86 -s build_type=Release --build=missing --output-folder=build
+conan install . -s arch=x86 -s compiler.cppstd=17 -s compiler.runtime=static -s build_type=Release --build=missing --output-folder=build
 cmake --preset conan-default
 cmake --build --preset conan-release
