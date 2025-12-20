20b1360ff5ce1c1573a5fc076e408ced84fa923d
Ben Walker <146777065+RedstonePilot@users.noreply.github.com>
2025-12-20 20:43:30 +0000

    clang-fomat

--- src/vSMRPlugIn.cpp
+++ src/vSMRPlugIn.cpp
@@ -12,53 +12,68 @@
 #include <windows.h>
 
 vSMRPlugIn::vSMRPlugIn()
-    : EuroScopePlugIn::CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE, PLUGIN_NAME,
-                               PLUGIN_VERSION, PLUGIN_DEVELOPERS,
-                               PLUGIN_COPYRIGHT) {
+    : EuroScopePlugIn::CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE,
+                               PLUGIN_NAME,
+                               PLUGIN_VERSION,
+                               PLUGIN_DEVELOPERS,
+                               PLUGIN_COPYRIGHT)
+{
 
-  try {
-    auto logPath = getDllPath() / "vSMR.log";
-    Logger::initialise(logPath);
-    Logger::getInstance().info("vSMR Plugin Initialised - Version " +
-                               std::string(PLUGIN_VERSION));
-  } catch (const std::exception &e) {
-    DisplayMessage("Failed to initialise vSMR logger: " + std::string(e.what()),
-                   "Error");
-  }
-  DisplayMessage("Version " + std::string(PLUGIN_VERSION) + " loaded",
-                 "Initialisation");
+    try
+    {
+        auto logPath = getDllPath() / "vSMR.log";
+        Logger::initialise(logPath);
+        Logger::getInstance().info("vSMR Plugin Initialised - Version " +
+                                   std::string(PLUGIN_VERSION));
+    }
+    catch (const std::exception & e)
+    {
+        DisplayMessage("Failed to initialise vSMR logger: " +
+                           std::string(e.what()),
+                       "Error");
+    }
+    DisplayMessage("Version " + std::string(PLUGIN_VERSION) + " loaded",
+                   "Initialisation");
 
-  // Register Displays with Euroscope
-  RegisterDisplayType(PLUGIN_DISPLAY_NAME, false, true, true, true);
+    // Register Displays with Euroscope
+    RegisterDisplayType(PLUGIN_DISPLAY_NAME, false, true, true, true);
 
-  // Register Tags with Euroscope
+    // Register Tags with Euroscope
 }
 
 vSMRPlugIn::~vSMRPlugIn() = default;
 
-void vSMRPlugIn::DisplayMessage(const std::string &message,
-                                const std::string &sender) {
-  DisplayUserMessage(PLUGIN_NAME, sender.c_str(), message.c_str(), true, false,
-                     false, false, false);
+void vSMRPlugIn::DisplayMessage(const std::string & message,
+                                const std::string & sender)
+{
+    DisplayUserMessage(PLUGIN_NAME, sender.c_str(), message.c_str(), true,
+                       false, false, false, false);
 }
 EuroScopePlugIn::CRadarScreen *
-vSMRPlugIn::OnRadarScreenCreated(const char *displayName, bool needRadarContent,
-                                 bool geoReferenced, bool canBeSaved,
-                                 bool canBeCreated) {
-  // Check to see if we are creating a vSMR display type
-  if (strcmp(displayName, PLUGIN_DISPLAY_NAME) == 0) {
-    Logger::getInstance().info("Created display: " + std::string(displayName));
-    // Create new Radar Screen here
-  }
+vSMRPlugIn::OnRadarScreenCreated(const char * displayName,
+                                 bool needRadarContent,
+                                 bool geoReferenced,
+                                 bool canBeSaved,
+                                 bool canBeCreated)
+{
+    // Check to see if we are creating a vSMR display type
+    if (strcmp(displayName, PLUGIN_DISPLAY_NAME) == 0)
+    {
+        Logger::getInstance().info("Created display: " +
+                                   std::string(displayName));
+        // Create new Radar Screen here
+    }
 }
 
-bool vSMRPlugIn::OnCompileCommand(const char *sCommandLine) {
-  if (startsWith(".smr hello", sCommandLine)) {
-    Logger::getInstance().info("Called Hello command");
-    DisplayUserMessage("vSMR", "vSMR", "Hello!", true, true, false, true,
-                       false);
-    return true;
-  }
+bool vSMRPlugIn::OnCompileCommand(const char * sCommandLine)
+{
+    if (startsWith(".smr hello", sCommandLine))
+    {
+        Logger::getInstance().info("Called Hello command");
+        DisplayUserMessage("vSMR", "vSMR", "Hello!", true, true, false, true,
+                           false);
+        return true;
+    }
 
-  return false;
+    return false;
 }
