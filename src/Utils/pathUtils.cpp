96e1895b431a8aee59c83a2dd16097c75233b298
Ben Walker <146777065+RedstonePilot@users.noreply.github.com>
2025-12-20 18:43:34 +0000

    add path utils

---
+++ src/Utils/pathUtils.cpp
@@ -0,0 +1,28 @@
+#include "pathUtils.hpp"
+
+#include <Windows.h>
+
+
+std::filesystem::path getDllPath()
+{
+    HMODULE hModule = nullptr;
+
+    if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | 
+                            GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, 
+                            reinterpret_cast<LPCSTR>(&getDllPath), &hModule))
+    {
+        char path[MAX_PATH];
+        if (GetModuleFileName(hModule, path, MAX_PATH))
+        {
+            std::filesystem::path dllPath{path};
+            return dllPath.parent_path();
+        }
+    }
+    auto currentPath = std::filesystem::current_path();
+    if (std::filesystem::exists(currentPath / "UK/Data/Plugin/vSMR/vSMR.dll"))
+    {
+        return currentPath / "UK/Data/Plugin/vSMR/vSMR.dll";
+    }
+    
+    return currentPath;
+}
